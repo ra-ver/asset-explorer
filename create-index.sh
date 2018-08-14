3e01f4f7be62c3f2f86277093aec72f3af135163
Rahul Verma <Rahul.Verma@oicr.on.ca>
2018-08-14 12:28:37 -0400

    Filters based on regex
    Adds regex for JPG, JPEG, PNG and SVG file types

--- create-index.sh
+++ create-index.sh
@@ -6,7 +6,7 @@ OUTPUT="src/images.json"
 
 i=0
 echo "[" > $OUTPUT
-for i in `find "$ROOT" -maxdepth 1 -mindepth 1 -type f -iname '*.svg' -iname '*.png' -iname '*.jpg'| sort`; do
+for i in `find -E "$ROOT" -maxdepth 1 -mindepth 1 -type f -regex ".*\.(jpg|gif|png|jpeg|svg)| sort`; do
     file=`basename "$i"`
     echo "{ \"src\":\"$HTTP$file\", \"thumbnail\":\"$HTTP$file\", \"thumbnailWidth\": 50, \"thumbnailHeight\":50, \"caption\":\"$file\", \"tags\":[{\"value\":\"$file\", \"title\":\"$file\"}] }," >> $OUTPUT
 done
