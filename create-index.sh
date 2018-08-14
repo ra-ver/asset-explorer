eb432bc8f2aab58fff0f049bed75b7181a426f7b
Rahul Verma <Rahul.Verma@oicr.on.ca>
2018-08-14 12:23:33 -0400

    Adds support for jpg and png files

--- create-index.sh
+++ create-index.sh
@@ -6,7 +6,7 @@ OUTPUT="src/images.json"
 
 i=0
 echo "[" > $OUTPUT
-for i in `find "$ROOT" -maxdepth 1 -mindepth 1 -type f -iname '*.svg'| sort`; do
+for i in `find "$ROOT" -maxdepth 1 -mindepth 1 -type f -iname '*.svg' -iname '*.png' -iname '*.jpg'| sort`; do
     file=`basename "$i"`
     echo "{ \"src\":\"$HTTP$file\", \"thumbnail\":\"$HTTP$file\", \"thumbnailWidth\": 50, \"thumbnailHeight\":50, \"caption\":\"$file\", \"tags\":[{\"value\":\"$file\", \"title\":\"$file\"}] }," >> $OUTPUT
 done
